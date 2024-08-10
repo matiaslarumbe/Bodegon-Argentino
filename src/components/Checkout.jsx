38ab5c65b6f7b1a2ca957b77e931dd5ba6d65fe0
Matias Larumbe <matias_larumbe@hotmail.com>
2024-08-10 19:45:45 -0300

    Creacion de botones de metodos de pago con iconos

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { RiVisaFill } from "react-icons/ri";
+import { FaCcMastercard } from "react-icons/fa6";
+import { FaMoneyBillAlt } from "react-icons/fa";
+import { SiMercadopago } from "react-icons/si";
 
 const Checkout = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const [submitted, setSubmitted] = useState(false);
+  const [paymentMethod, setPaymentMethod] = useState('');
 
   const onSubmit = (data) => {
-    console.log(data);
-    setSubmitted(true); // Cambiar el estado a 'true' para mostrar el mensaje
+    console.log({ ...data, paymentMethod });
+    setSubmitted(true);
   };
 
   return (
@@ -50,6 +55,43 @@ const Checkout = () => {
             />
             {errors.telefono && <span className="text-red-500">Este campo es requerido</span>}
           </div>
+
+          <div className="mb-6">
+            <label className="block text-gray-400 mb-2">Método de Pago:</label>
+            <div className="flex gap-4">
+              <button
+                type="button"
+                onClick={() => setPaymentMethod('Visa')}
+                className={`p-4 rounded-lg ${paymentMethod === 'Visa' ? 'bg-[#1F1D2B]' : ''}`}
+              >
+                <RiVisaFill size={36} />
+              </button>
+              <button
+                type="button"
+                onClick={() => setPaymentMethod('Mastercard')}
+                className={`p-4 rounded-lg ${paymentMethod === 'Mastercard' ? 'bg-[#1F1D2B]' : ''}`}
+              >
+                <FaCcMastercard size={36} />
+              </button>
+              <button
+                type="button"
+                onClick={() => setPaymentMethod('Efectivo')}
+                className={`p-4 rounded-lg ${paymentMethod === 'Efectivo' ? 'bg-[#1F1D2B]' : ''}`}
+              >
+                 <FaMoneyBillAlt size={36} />
+                
+              </button>
+              <button
+                type="button"
+                onClick={() => setPaymentMethod('MercadoPago')}
+                className={`p-4 rounded-lg ${paymentMethod === 'MercadoPago' ? 'bg-[#1F1D2B]' : ''}`}
+              >
+               <SiMercadopago size={36} />
+              </button>
+            </div>
+            {paymentMethod === '' && <span className="text-red-500">Seleccione un método de pago</span>}
+          </div>
+
           <button
             type="submit"
             className="bg-[#28cfe7] text-white py-2 px-4 rounded-lg w-full"
@@ -63,5 +105,6 @@ const Checkout = () => {
 };
 
 export default Checkout;
+
 
 

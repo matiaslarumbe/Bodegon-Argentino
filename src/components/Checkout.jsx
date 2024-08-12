9324ca3c18cde98c62460c6b7d2723fb6a6f064a
Matias Larumbe <matias_larumbe@hotmail.com>
2024-08-11 23:10:09 -0300

    Actualizacion para dispositivo responsive

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,3 +1,111 @@
+// import React, { useState } from 'react';
+// import { useForm } from 'react-hook-form';
+// import { RiVisaFill } from "react-icons/ri";
+// import { FaCcMastercard } from "react-icons/fa6";
+// import { FaMoneyBillAlt } from "react-icons/fa";
+// import { SiMercadopago } from "react-icons/si";
+
+// const Checkout = () => {
+//   const { register, handleSubmit, formState: { errors } } = useForm();
+//   const [submitted, setSubmitted] = useState(false);
+//   const [paymentMethod, setPaymentMethod] = useState('');
+
+//   const onSubmit = (data) => {
+//     console.log({ ...data, paymentMethod });
+//     setSubmitted(true);
+//   };
+
+//   return (
+//     <div className="p-8 min-h-screen bg-[#262837] text-gray-300">
+//       <h1 className="text-3xl mb-8">Finalizar la Compra</h1>
+      
+//       {submitted ? (
+//         <div className="text-center text-green-500 text-2xl">
+//           ¡Gracias por su compra!
+//         </div>
+//       ) : (
+//         <form onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto">
+//           <div className="mb-4">
+//             <label htmlFor="nombre" className="block text-gray-400 mb-2">Nombre:</label>
+//             <input
+//               type="text"
+//               id="nombre"
+//               {...register('nombre', { required: true })}
+//               className="w-full p-2 bg-[#1F1D2B] rounded-lg outline-none"
+//             />
+//             {errors.nombre && <span className="text-red-500">Este campo es requerido</span>}
+//           </div>
+//           <div className="mb-4">
+//             <label htmlFor="email" className="block text-gray-400 mb-2">Email:</label>
+//             <input
+//               type="email"
+//               id="email"
+//               {...register('email', { required: true })}
+//               className="w-full p-2 bg-[#1F1D2B] rounded-lg outline-none"
+//             />
+//             {errors.email && <span className="text-red-500">Este campo es requerido</span>}
+//           </div>
+//           <div className="mb-4">
+//             <label htmlFor="telefono" className="block text-gray-400 mb-2">Teléfono:</label>
+//             <input
+//               type="tel"
+//               id="telefono"
+//               {...register('telefono', { required: true })}
+//               className="w-full p-2 bg-[#1F1D2B] rounded-lg outline-none"
+//             />
+//             {errors.telefono && <span className="text-red-500">Este campo es requerido</span>}
+//           </div>
+
+//           <div className="mb-6">
+//             <label className="block text-gray-400 mb-2">Método de Pago:</label>
+//             <div className="flex gap-4">
+//               <button
+//                 type="button"
+//                 onClick={() => setPaymentMethod('Visa')}
+//                 className={`p-4 rounded-lg ${paymentMethod === 'Visa' ? 'bg-[#1F1D2B]' : ''}`}
+//               >
+//                 <RiVisaFill size={36} />
+//               </button>
+//               <button
+//                 type="button"
+//                 onClick={() => setPaymentMethod('Mastercard')}
+//                 className={`p-4 rounded-lg ${paymentMethod === 'Mastercard' ? 'bg-[#1F1D2B]' : ''}`}
+//               >
+//                 <FaCcMastercard size={36} />
+//               </button>
+//               <button
+//                 type="button"
+//                 onClick={() => setPaymentMethod('Efectivo')}
+//                 className={`p-4 rounded-lg ${paymentMethod === 'Efectivo' ? 'bg-[#1F1D2B]' : ''}`}
+//               >
+//                  <FaMoneyBillAlt size={36} />
+                
+//               </button>
+//               <button
+//                 type="button"
+//                 onClick={() => setPaymentMethod('MercadoPago')}
+//                 className={`p-4 rounded-lg ${paymentMethod === 'MercadoPago' ? 'bg-[#1F1D2B]' : ''}`}
+//               >
+//                <SiMercadopago size={36} />
+//               </button>
+//             </div>
+//             {paymentMethod === '' && <span className="text-red-500">Seleccione un método de pago</span>}
+//           </div>
+
+//           <button
+//             type="submit"
+//             className="bg-[#28cfe7] text-white py-2 px-4 rounded-lg w-full"
+//           >
+//             Finalizar Compra
+//           </button>
+//         </form>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Checkout;
+
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { RiVisaFill } from "react-icons/ri";
@@ -16,85 +124,84 @@ const Checkout = () => {
   };
 
   return (
-    <div className="p-8 min-h-screen bg-[#262837] text-gray-300">
-      <h1 className="text-3xl mb-8">Finalizar la Compra</h1>
+    <div className="p-4 lg:p-8 min-h-screen bg-[#262837] text-gray-300">
+      <h1 className="text-2xl lg:text-3xl mb-6 lg:mb-8">Finalizar la Compra</h1>
       
       {submitted ? (
-        <div className="text-center text-green-500 text-2xl">
+        <div className="text-center text-green-500 text-xl lg:text-2xl">
           ¡Gracias por su compra!
         </div>
       ) : (
-        <form onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto">
+        <form onSubmit={handleSubmit(onSubmit)} className="max-w-xs lg:max-w-md mx-auto">
           <div className="mb-4">
-            <label htmlFor="nombre" className="block text-gray-400 mb-2">Nombre:</label>
+            <label htmlFor="nombre" className="block text-gray-400 mb-2 text-sm lg:text-base">Nombre:</label>
             <input
               type="text"
               id="nombre"
               {...register('nombre', { required: true })}
-              className="w-full p-2 bg-[#1F1D2B] rounded-lg outline-none"
+              className="w-full p-2 bg-[#1F1D2B] rounded-lg outline-none text-sm lg:text-base"
             />
-            {errors.nombre && <span className="text-red-500">Este campo es requerido</span>}
+            {errors.nombre && <span className="text-red-500 text-xs lg:text-sm">Este campo es requerido</span>}
           </div>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-400 mb-2">Email:</label>
+            <label htmlFor="email" className="block text-gray-400 mb-2 text-sm lg:text-base">Email:</label>
             <input
               type="email"
               id="email"
               {...register('email', { required: true })}
-              className="w-full p-2 bg-[#1F1D2B] rounded-lg outline-none"
+              className="w-full p-2 bg-[#1F1D2B] rounded-lg outline-none text-sm lg:text-base"
             />
-            {errors.email && <span className="text-red-500">Este campo es requerido</span>}
+            {errors.email && <span className="text-red-500 text-xs lg:text-sm">Este campo es requerido</span>}
           </div>
           <div className="mb-4">
-            <label htmlFor="telefono" className="block text-gray-400 mb-2">Teléfono:</label>
+            <label htmlFor="telefono" className="block text-gray-400 mb-2 text-sm lg:text-base">Teléfono:</label>
             <input
               type="tel"
               id="telefono"
               {...register('telefono', { required: true })}
-              className="w-full p-2 bg-[#1F1D2B] rounded-lg outline-none"
+              className="w-full p-2 bg-[#1F1D2B] rounded-lg outline-none text-sm lg:text-base"
             />
-            {errors.telefono && <span className="text-red-500">Este campo es requerido</span>}
+            {errors.telefono && <span className="text-red-500 text-xs lg:text-sm">Este campo es requerido</span>}
           </div>
 
           <div className="mb-6">
-            <label className="block text-gray-400 mb-2">Método de Pago:</label>
-            <div className="flex gap-4">
+            <label className="block text-gray-400 mb-2 text-sm lg:text-base">Método de Pago:</label>
+            <div className="flex flex-wrap gap-4">
               <button
                 type="button"
                 onClick={() => setPaymentMethod('Visa')}
-                className={`p-4 rounded-lg ${paymentMethod === 'Visa' ? 'bg-[#1F1D2B]' : ''}`}
+                className={`p-3 lg:p-4 rounded-lg ${paymentMethod === 'Visa' ? 'bg-[#1F1D2B]' : ''}`}
               >
-                <RiVisaFill size={36} />
+                <RiVisaFill size={28} lg={36} />
               </button>
               <button
                 type="button"
                 onClick={() => setPaymentMethod('Mastercard')}
-                className={`p-4 rounded-lg ${paymentMethod === 'Mastercard' ? 'bg-[#1F1D2B]' : ''}`}
+                className={`p-3 lg:p-4 rounded-lg ${paymentMethod === 'Mastercard' ? 'bg-[#1F1D2B]' : ''}`}
               >
-                <FaCcMastercard size={36} />
+                <FaCcMastercard size={28} lg={36} />
               </button>
               <button
                 type="button"
                 onClick={() => setPaymentMethod('Efectivo')}
-                className={`p-4 rounded-lg ${paymentMethod === 'Efectivo' ? 'bg-[#1F1D2B]' : ''}`}
+                className={`p-3 lg:p-4 rounded-lg ${paymentMethod === 'Efectivo' ? 'bg-[#1F1D2B]' : ''}`}
               >
-                 <FaMoneyBillAlt size={36} />
-                
+                <FaMoneyBillAlt size={28} lg={36} />
               </button>
               <button
                 type="button"
                 onClick={() => setPaymentMethod('MercadoPago')}
-                className={`p-4 rounded-lg ${paymentMethod === 'MercadoPago' ? 'bg-[#1F1D2B]' : ''}`}
+                className={`p-3 lg:p-4 rounded-lg ${paymentMethod === 'MercadoPago' ? 'bg-[#1F1D2B]' : ''}`}
               >
-               <SiMercadopago size={36} />
+                <SiMercadopago size={28} lg={36} />
               </button>
             </div>
-            {paymentMethod === '' && <span className="text-red-500">Seleccione un método de pago</span>}
+            {paymentMethod === '' && <span className="text-red-500 text-xs lg:text-sm">Seleccione un método de pago</span>}
           </div>
 
           <button
             type="submit"
-            className="bg-[#28cfe7] text-white py-2 px-4 rounded-lg w-full"
+            className="bg-[#28cfe7] text-white py-2 px-4 rounded-lg w-full text-sm lg:text-base"
           >
             Finalizar Compra
           </button>
@@ -105,6 +212,5 @@ const Checkout = () => {
 };
 
 export default Checkout;
-
 
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,54 @@
+// import React from 'react';
+// import { RiSearchLine } from 'react-icons/ri';
+
+// const Header = ({ cambiarCategoria, categoriaSeleccionada, setBusqueda }) => {
+//   const categorias = ['Todos', 'Carnes', 'Pastas', 'Verduras y ensaladas', 'Bebidas'];
+
+//   const manejarBusqueda = (e) => {
+//     setBusqueda(e.target.value);
+//   };
+
+//   return (
+//     <header>
+//       <div className="flex flex-col md:flex-row md:justify-between md:item-center gap-4 mb-6">
+//         <div>
+//           <h1 className="text-2xl text-gray-300">Bodegon Argentino</h1>
+//           <p className="text-gray-500">16 abril 2024</p>
+//         </div>
+//         <form>
+//           <div className="w-full relative">
+//             <RiSearchLine className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-300" />
+//             <input
+//               type="text"
+//               className="bg-[#1F1D2B] w-full py-2 pl-10 pr-4 rounded-lg text-gray-300 outline-none"
+//               placeholder="Busqueda"
+//               onChange={manejarBusqueda} // Captura el evento onChange
+//             />
+//           </div>
+//         </form>
+//       </div>
+//       <nav className="text-gray-300 flex items-center justify-between md:justify-start md:gap-8 border-b mb-6">
+//         {categorias.map((categoria) => (
+//           <button
+//             key={categoria}
+//             onClick={() => cambiarCategoria(categoria)}
+//             className={`relative py-2 pr-4 before:w-full before:h-[2px] before:absolute before:left-0 before:rounded-full before:-bottom-[1px] ${
+//               categoriaSeleccionada === categoria
+//                 ? 'text-[#28cfe7] before:bg-[#28cfe7] font-bold'
+//                 : 'text-gray-300 before:bg-transparent'
+//             }`}
+//           >
+//             {categoria}
+//           </button>
+//         ))}
+//       </nav>
+//     </header>
+//   );
+// };
+
+// export default Header;
+
+
 import React from 'react';
 import { RiSearchLine } from 'react-icons/ri';
 
@@ -10,24 +61,24 @@ const Header = ({ cambiarCategoria, categoriaSeleccionada, setBusqueda }) => {
 
   return (
     <header>
-      <div className="flex flex-col md:flex-row md:justify-between md:item-center gap-4 mb-6">
+      <div className="flex flex-col gap-4 mb-6 md:flex-row md:justify-between md:items-center">
         <div>
           <h1 className="text-2xl text-gray-300">Bodegon Argentino</h1>
           <p className="text-gray-500">16 abril 2024</p>
         </div>
-        <form>
-          <div className="w-full relative">
+        <form className="w-full md:w-1/3 lg:w-1/4">
+          <div className="relative">
             <RiSearchLine className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-300" />
             <input
               type="text"
               className="bg-[#1F1D2B] w-full py-2 pl-10 pr-4 rounded-lg text-gray-300 outline-none"
-              placeholder="Busqueda"
-              onChange={manejarBusqueda} // Captura el evento onChange
+              placeholder="Buscar"
+              onChange={manejarBusqueda}
             />
           </div>
         </form>
       </div>
-      <nav className="text-gray-300 flex items-center justify-between md:justify-start md:gap-8 border-b mb-6">
+      <nav className="flex flex-wrap gap-4 text-gray-300 border-b border-gray-700 mb-6 md:gap-8 md:justify-start">
         {categorias.map((categoria) => (
           <button
             key={categoria}
@@ -47,5 +98,3 @@ const Header = ({ cambiarCategoria, categoriaSeleccionada, setBusqueda }) => {
 };
 
 export default Header;
-
-

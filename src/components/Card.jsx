7af4e8182ca01e5c0b35e2da0327566d21f70714
Matias Larumbe <matias_larumbe@hotmail.com>
2024-08-13 10:37:38 -0300

    Actualizacion en Item.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,9 +8,18 @@ const Card = ({ platos }) => {
     <div>
       <div className="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
         {platos.map((plato, index) => (
-          <div key={index} className="bg-[#1F1D2B] p-8 rounded-xl gap-2 flex flex-col items-center text-center text-gray-300">
-            <img src={plato.imagen} className="w-40 h-40 object-cover -mt-20 shadow-2xl rounded-full" alt={plato.nombre} />
-            <p className="text-xl">{plato.nombre}</p>
+          <div
+            key={index}
+            className="bg-[#1F1D2B] p-8 rounded-xl flex flex-col items-center text-center text-gray-300"
+          >
+            <div className="w-40 h-40 overflow-hidden rounded-full -mt-20 shadow-2xl">
+              <img
+                src={plato.imagen}
+                className="w-full h-full object-cover"
+                alt={plato.nombre}
+              />
+            </div>
+            <p className="text-xl mt-4">{plato.nombre}</p>
             <span className="text-gray-400">${plato.precio}</span>
             <p className="text-gray-600">{plato.disponibles} Platos Disponibles</p>
             <button
@@ -27,5 +36,3 @@ const Card = ({ platos }) => {
 };
 
 export default Card;
-
-
